0df51b91b24a3766d3152e787289dc3730fab5f5
cbot918 <cbot918@gmail.com>
2023-07-01 17:22:15 +0800

    update

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/cbot918/gob
 
 go 1.19
+
+require github.com/cbot918/tools v1.0.0
---
+++ installer/install_linux.sh
@@ -0,0 +1,3 @@
+curl -OL https://github.com/cbot918/gob/releases/download/v1.0.1/gob
+sudo mv gob /usr/local/bin
+echo "install successful"
